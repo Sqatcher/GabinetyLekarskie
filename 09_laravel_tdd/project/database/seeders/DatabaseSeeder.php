dc29b8eb1f1d67ffde3a1d5d82e5c4206edd0261
pelek20 <kacpermuller@student.agh.edu.pl>
2023-04-01 11:49:51 +0200

    doprowadzenie do dzialania

--- 09_laravel_tdd/project/database/seeders/DatabaseSeeder.php
+++ 09_laravel_tdd/project/database/seeders/DatabaseSeeder.php
@@ -3,19 +3,21 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeders.
      */
     public function run(): void
     {
-        $this->call(UsersSeeder::class);
-        $this->call(SpecialEventsSeeder::class);
-        $this->call(EventsSeeder::class);
-        $this->call(BlikCodesSeeder::class);
-        $this->call(BetEventsSeeder::class);
-        $this->call(BetsSeeder::class);
+        /*DB::table('users')->insert([
+            'name' => Str::random(10),
+            'email' => Str::random(10).'@gmail.com',
+            'password' => Hash::make('password'),
+        ]);*/
     }
 }
