64546930aa54fb358f6d7875b856006331986ca2
pelek20 <kacpermuller@student.agh.edu.pl>
2023-04-01 16:26:18 +0200

    bazy danych

--- 09_laravel_tdd/project/database/seeders/DatabaseSeeder.php
+++ 09_laravel_tdd/project/database/seeders/DatabaseSeeder.php
@@ -3,21 +3,14 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Run the database seeders.
+     * Seed the application's database.
      */
     public function run(): void
     {
-        /*DB::table('users')->insert([
-            'name' => Str::random(10),
-            'email' => Str::random(10).'@gmail.com',
-            'password' => Hash::make('password'),
-        ]);*/
+        $this->call(UsersSeeder::class);
     }
 }
---
+++ 09_laravel_tdd/project/database/seeders/UsersSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            'email' => 'first.name@email.com',
+            'password' => bcrypt('first'),
+        ]);
+    }
+}
