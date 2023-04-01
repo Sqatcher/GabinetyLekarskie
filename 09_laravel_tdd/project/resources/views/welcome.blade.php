b07fd496477db410be0474a7092ccc696ff4344b
pelek20 <kacpermuller@student.agh.edu.pl>
2023-04-01 14:14:57 +0200

    basics

--- 09_laravel_tdd/project/resources/views/welcome.blade.php
+++ 09_laravel_tdd/project/resources/views/welcome.blade.php
@@ -1,1 +1,21 @@
-hi
+<div style="width:1000px; margin:auto; margin-top: 100px">
+
+    <div style = "width: fit-content;margin:auto">
+        <a style="font-size: 48px; font-family: Arial">Gabinety</a>
+    </div>
+
+    <div style="width:fit-content; margin: auto; margin-top: 300px;">
+        <form>
+          <div>
+            <label for="email">Email</label><br>
+            <input type="email" style="margin:5px 0 15px 0;" id="email" placeholder="jan.jan@gabinety.com" required>
+          </div>
+          <div>
+            <label for="password">Hasło</label><br>
+            <input type="password" id="password" style="margin-top:5px;" placeholder="hasło" required>
+          </div><br>
+          <button type="submit">Zaloguj się</button>
+        </form>
+    </div>
+
+</div>
