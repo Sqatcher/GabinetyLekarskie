49713a129b9b3e86349a5e1e3c3b815ccacbd0a7
serwanska <serwanska@student.agh.edu.pl>
2023-04-24 22:20:40 +0200

    Backgroung image

--- project/resources/views/dashboard.blade.php
+++ project/resources/views/dashboard.blade.php
@@ -1,3 +1,14 @@
 <x-app-layout>
-    Witaj!
+
+    <div style="background-image:url('./img/dr.jpg'); height:100vh; width:100vw; background-position: center; background-repeat: no-repeat; background-size: cover; margin: auto;">
+
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6" style="padding-left: 0vw; padding-top: 40vh; width: 100vw; height: 20vh; padding-right: 0vw; padding-bottom: 40vh;" >
+            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg" style="" >
+                Witaj!
+            </div>
+        </div>
+    </div>
 </x-app-layout>
+<!--
+background-size: contain;height: 500px;width: 890px; margin: auto; margin-top: 50px
+-->
