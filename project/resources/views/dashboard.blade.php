acb4c428580292aef8af6a8f09ffe56cf6023078
serwanska <serwanska@student.agh.edu.pl>
2023-04-25 21:17:29 +0200

    done

--- project/resources/views/dashboard.blade.php
+++ project/resources/views/dashboard.blade.php
@@ -1,14 +1,32 @@
 <x-app-layout>
 
-    <div style="background-image:url('./img/dr.jpg'); height:100vh; width:100vw; background-position: center; background-repeat: no-repeat; background-size: cover; margin: auto;">
+    <div style="background-image:url('./img/dr.png'); height:100vh; width:100vw; background-position: center; background-repeat: no-repeat; background-size: cover; margin: auto;">
 
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6" style="padding-left: 0vw; padding-top: 40vh; width: 100vw; height: 20vh; padding-right: 0vw; padding-bottom: 40vh;" >
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg" style="" >
-                Witaj!
-            </div>
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6" style="padding-left: 0vw; padding-top: 20vh; width: 100vw; height: 20vh; padding-right: 0vw; padding-bottom: 40vh;" >
+
+
         </div>
     </div>
 </x-app-layout>
 <!--
 background-size: contain;height: 500px;width: 890px; margin: auto; margin-top: 50px
+ <div class="p-4 sm:p-8 bg-white sm:rounded-lg" style="background-color: rgba(255,255,255,0); border: 1px solid rgba(255,255,255,0); font-size: 35px; font-weight: 800; font-family: 'Poppins', sans-serif;" >
+                <p  style=" position: absolute;
+                color: rgba(255, 255, 255, 1);
+                font-size: 10rem;
+                line-height: 0;
+                bottom: 325px;
+                right: 30px; "> ”</p>
+                <p style="opacity: 1; text-align: center; position: relative;
+    color: #ffffff;
+    padding: 30px 0;
+    width: 100%;
+    max-width: 500px;
+    z-index: 1;
+    margin: 80px auto;
+    align-self: center;
+    border-top: solid 1px;
+    border-bottom: solid 1px;"></p>
+
+            </div>
 -->
