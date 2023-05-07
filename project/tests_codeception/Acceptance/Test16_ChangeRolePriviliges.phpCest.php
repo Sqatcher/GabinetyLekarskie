5d97b07ee074aa261ac762e575938aaeffa66fa9
pelek20 <kacpermuller@student.agh.edu.pl>
2023-05-07 19:22:35 +0200

    Vice versa tests

--- project/tests_codeception/Acceptance/Test16_ChangeRolePriviliges.phpCest.php
+++ project/tests_codeception/Acceptance/Test16_ChangeRolePriviliges.phpCest.php
@@ -39,15 +39,49 @@ class Test16_ChangeRolePriviligesCest
             $I->checkOption('input[id="edit_storage'.$i.'"]');
         }
 
-
         $I->fillField("current_password", "administrator");
         $I->click('Zapisz');
+
 
         $role = ['Kierownik', 'Pracownik', 'Księgowy', 'Magazynier', 'Recepcjonista'];
         foreach($role as $rola) {
             $I->seeInDatabase("roles", ['name' => $rola, 'users' => 31]);
             $I->seeInDatabase("roles", ['name' => $rola, 'schedules' => 31]);
             $I->seeInDatabase("roles", ['name' => $rola, 'storage' => 15]);
+        }
+
+
+
+        $I->amOnPage('/roles');
+
+        for($i=2; $i<=6; ++$i){
+            $I->uncheckOption('input[id="see_user'.$i.'"]');
+            $I->uncheckOption('input[id="create_user'.$i.'"]');
+            $I->uncheckOption('input[id="create_facility_user'.$i.'"]');
+            $I->uncheckOption('input[id="edit_user'.$i.'"]');
+            $I->uncheckOption('input[id="edit_facility_user'.$i.'"]');
+
+            $I->uncheckOption('input[id="see_schedules'.$i.'"]');
+            $I->uncheckOption('input[id="create_schedules'.$i.'"]');
+            $I->uncheckOption('input[id="create_facility_schedules'.$i.'"]');
+            $I->uncheckOption('input[id="edit_schedules'.$i.'"]');
+            $I->uncheckOption('input[id="edit_facility_schedules'.$i.'"]');
+
+            $I->uncheckOption('input[id="see_facility_storage'.$i.'"]');
+            $I->uncheckOption('input[id="see_storage'.$i.'"]');
+            $I->uncheckOption('input[id="edit_facility_storage'.$i.'"]');
+            $I->uncheckOption('input[id="edit_storage'.$i.'"]');
+        }
+
+        $I->fillField("current_password", "administrator");
+        $I->click('Zapisz');
+
+
+        $role = ['Kierownik', 'Pracownik', 'Księgowy', 'Magazynier', 'Recepcjonista'];
+        foreach($role as $rola) {
+            $I->seeInDatabase("roles", ['name' => $rola, 'users' => 0]);
+            $I->seeInDatabase("roles", ['name' => $rola, 'schedules' => 0]);
+            $I->seeInDatabase("roles", ['name' => $rola, 'storage' => 0]);
         }
     }
 }
